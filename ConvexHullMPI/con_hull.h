b0ec7f9987dc2374378b9922686661a1cdc1e9b6
areebaawan <93518118+areebaawan@users.noreply.github.com>
2024-12-03 20:18:15 +0500

    Add files via upload

---
+++ ConvexHullMPI/con_hull.h
@@ -0,0 +1,7 @@
+#ifndef CON_HULL_H
+#define CON_HULL_H
+
+void findHull(int x[], int y[], int n, int *convx, int *convy, int *n_c);
+
+#endif
+
---
+++ ConvexHullOMP/kMeans.h
@@ -0,0 +1,7 @@
+#ifndef KMEANS_H
+#define KMEANS_H
+
+void find_clusters(int n, int k, int *x, int *y, int *kc);
+
+#endif
+
